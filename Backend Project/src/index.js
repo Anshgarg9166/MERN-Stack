bb555f270fe32585bfa23b4c926cedd1f27f6685
Ansh garg <gargansh2712@gmail.com>
2024-08-21 02:15:58 +0530

    utils addition

--- Backend Project/src/index.js
+++ Backend Project/src/index.js
@@ -8,6 +8,15 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
 
 
 /*
@@ -16,7 +25,7 @@ const app = express()
 ;( async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("errror", (error) => {
+        app.on("error", (error) => {
             console.log("ERRR: ", error);
             throw error
         })
